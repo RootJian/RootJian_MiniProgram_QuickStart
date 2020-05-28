63c0fa4ae0d004ec35590099d8c49e8948b26012
黄大浪 <454285770@qq.com>
2020-05-28 14:51:29 +0800

    Update Request.js

--- utils/Request.js
+++ utils/Request.js
@@ -1,5 +1,17 @@
 import { path } from '../config';
 const errorIcon = "/images/icon/error.png";
+/**
+ * 
+ * @function Request 封装网络请求
+ * @description 对请求进行封装判断并抛出对应错误
+ * @param url {string} Api地址
+ * @param data {object} 需要传递到服务器的Json对象数据
+ * @param method {string} 请求方式
+ * @param success {function} 请求成功响应方法，返回服务器data数据
+ * @param fail {function} 请求失败响应方法，返回请求res对象
+ * @param complete {function} 请求完毕响应方法，返回请求res对象
+ * @author RootJian 2020/05/28
+ */
 function Request({ url, data, method, success, fail, complete }) {
   wx.showLoading({
     title: '加载中',
